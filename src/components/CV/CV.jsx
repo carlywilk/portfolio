3e28b4d4f19b2c7f886845127b25d6956a3b9de5
Carly Wilk <carlymwilk@gmail.com>
2024-06-16 02:32:17 -0400

    Fix styling for mobile width

--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -1,26 +1,9 @@
 
-import { useState } from "react";
-
 import cwHeadshot from "../../assets/images/carly-wilk-headshot.jpeg";
 
 import "./CV.scss";
 
 export function CV() {
-
-    const [selectedTab, setSelectedTab] = useState("skills");
-
-    const handleTabClick = (tab) => {
-        setSelectedTab(tab);
-    }
-
-    // const downloadCV = () => {
-    //     const link = document.createElement("a");
-    //     link.href = "" // link to cv
-    //     link.download = "carly-wilk-resume.pdf";
-    //     document.body.appendChild(link);
-    //     link.click();
-    //     document.body.removeChild(link);
-    // }
 
     return (
         <section className="cv">
@@ -30,27 +13,20 @@ export function CV() {
                     <h2 className="cv__about-title">About Me</h2>
                     <p className="cv__about-me-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius minima exercitationem alias delectus, voluptatum distinctio sapiente quis saepe eveniet voluptatem, excepturi veritatis dignissimos tempore ea natus accusantium aliquam blanditiis cum.</p>
                 </div>
-                <div className="cv__about-subsections">
-                    <div className="cv__about-subheader">
-                        <div className={selectedTab === "cv__skills" ? "active" : ""} onClick={() => handleTabClick("skills")}>Skills</div>
-                        <div className={selectedTab === "cv__experience" ? "active" : ""} onClick={() => handleTabClick("experience")}>Experience</div>
-                        <div className={selectedTab === "cv__education" ? "active" : ""} onClick={() => handleTabClick("education")}>Education</div>
+                <div className="cv__about-subsection">
+                    <div className="cv__subsection-title">Skills</div>
+                    <p className="cv__subsection-skills-content">HTML, CSS, Sass, JavaScript, React.js, Node.js, Express.js, Git, MySQL, API Data Handling</p>
+                </div>
+                <div className="cv__subsections-container">
+                    <div className="cv__about-subsection">
+                        <div className="cv__subsection-title">Experience</div>
+                        <p className="cv__subsection-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facere cupiditate nulla, veniam recusandae repellat incidunt eligendi dolor magnam aperiam? Qui odit, similique explicabo tempora non commodi repellat repellendus adipisci.</p>
                     </div>
-                    <div className="cv__about-content">
-                        {selectedTab === "skills" && (
-                            <div className="cv__content-skills">HTML, CSS, <br></br>Sass, JavaScript, React.js, MySQL, Node.js, Express.js, Git, API Data Handling</div>
-                        )}
-                        {selectedTab === "experience" && (
-                            <div className="cv__content-experience">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloribus tenetur modi iusto repellat sed libero quod veniam amet natus sequi quisquam eveniet assumenda numquam velit excepturi, rerum rem reprehenderit?</div>
-                        )}
-                        {selectedTab === "education" && (
-                            <div className="cv__content-education">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non iure, illum adipisci at debitis atque, alias illo amet officiis, culpa unde aspernatur! Veritatis inventore corrupti velit nam itaque excepturi nulla?</div>
-                        )}
+                    <div className="cv__about-subsection">
+                        <div className="cv__subsection-title">Education</div>
+                        <p className="cv__subsection-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facere cupiditate nulla, veniam recusandae repellat incidunt eligendi dolor magnam aperiam? Qui odit, similique explicabo tempora non commodi repellat repellendus adipisci.</p>
                     </div>
                 </div>
-                {/* <div className="cv__download">
-                    <button className="cv__dl-button">Download CV</button>
-                </div> */}
             </section>
         </section>
     );
