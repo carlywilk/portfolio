3de0c386f623b5857f1c0c9ad6c5001a9fb41ca5
Carly Wilk <carlymwilk@gmail.com>
2024-06-16 15:34:23 -0400

    Update cv section, update mobile styling

--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -11,7 +11,7 @@ export function CV() {
             <section className="cv__main">
                 <div className="cv__about">
                     <h2 className="cv__about-title">About Me</h2>
-                    <p className="cv__about-me-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius minima exercitationem alias delectus, voluptatum distinctio sapiente quis saepe eveniet voluptatem, excepturi veritatis dignissimos tempore ea natus accusantium aliquam blanditiis cum.</p>
+                    <p className="cv__about-me-content">With studies focused primarily in biological sciences, my experiences have led me to equity seeking and intersectional perspectives that foster connections of care and consent. Currently I am seeking ways to leverage my background in biology to aid with my transition into the tech industry as a software engineer.</p>
                 </div>
                 <div className="cv__about-subsection">
                     <div className="cv__subsection-title">Skills</div>
@@ -19,12 +19,22 @@ export function CV() {
                 </div>
                 <div className="cv__subsections-container">
                     <div className="cv__about-subsection">
-                        <div className="cv__subsection-title">Experience</div>
-                        <p className="cv__subsection-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facere cupiditate nulla, veniam recusandae repellat incidunt eligendi dolor magnam aperiam? Qui odit, similique explicabo tempora non commodi repellat repellendus adipisci.</p>
+                        <div className="cv__subsection-title">Education</div>
+                        <h3 className="cv__subsection-content-subheader">BrainStation | Software Engineering</h3>
+                        <p className="cv__subsection-content">Diploma, Mar 2024 - June 2024</p>
+                        <h3 className="cv__subsection-content-subheader--padded">TMU | Ethics</h3>
+                        <p className="cv__subsection-content">Certificate, May 2021 - Aug 2022</p>
+                        <h3 className="cv__subsection-content-subheader--padded">Carleton University | Biology</h3>
+                        <p className="cv__subsection-content">B.Sc. Honours, Sep 2015 - Aug 2020</p>
                     </div>
                     <div className="cv__about-subsection">
-                        <div className="cv__subsection-title">Education</div>
-                        <p className="cv__subsection-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facere cupiditate nulla, veniam recusandae repellat incidunt eligendi dolor magnam aperiam? Qui odit, similique explicabo tempora non commodi repellat repellendus adipisci.</p>
+                        <div className="cv__subsection-title">Experience</div>
+                        <h3 className="cv__subsection-content-subheader">Executive Assistant | Virtual Gurus</h3>
+                        <p className="cv__subsection-content">Remote | Jan 2022 - present</p>
+                        <h3 className="cv__subsection-content-subheader--padded">Sales Associate | Brock's</h3>
+                        <p className="cv__subsection-content">Port Perry, ON | Jul 2021 - Dec 2021</p>
+                        <h3 className="cv__subsection-content-subheader--padded">Student Partner | Carleton University</h3>
+                        <p className="cv__subsection-content">Remote, Jul 2020 - Aug 2020</p>
                     </div>
                 </div>
             </section>
