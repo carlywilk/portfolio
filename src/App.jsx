19ee210941b050a0ecfc38b7fc00dd8b46e3c5ef
Carly Wilk <carlymwilk@gmail.com>
2024-06-16 02:35:52 -0400

    Remove Footer component code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage/HomePage.jsx';
 import { PageNotFound } from "./pages/PageNotFound/PageNotFound.jsx";
 import { Header } from './components/Header/Header.jsx';
-import { Footer } from './components/Footer/Footer.jsx';
 
 import './App.scss';
 
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
